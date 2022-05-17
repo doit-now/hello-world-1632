a6cadea2bfb3c29df1c95965d123ddb005439dc8
doit-now <hoang.nguyenthe@gmail.com>
2022-05-17 15:24:13 +0700

    1st time to play with git

---
+++ src/hello/world/pkg1632/HelloWorld1632.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package hello.world.pkg1632;
+
+/**
+ *
+ * @author giao.lang
+ */
+public class HelloWorld1632 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello Java and GitHub. Summer 2022");
+    }
+    
+}
